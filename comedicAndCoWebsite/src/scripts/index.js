17dde593eee2ac4441f9ad107207061e5e17d165
comedicomedy <88862122+comedicomedy@users.noreply.github.com>
2024-07-24 01:31:39 -0400

    Moved around Files. Added Employee Contract Page with IRS background. Added a little bit to the home page of the website. TODO: Work on number of visitors counter. Probably somethind to do with storing data on the server.

--- comedicAndCoWebsite/src/scripts/index.js
+++ comedicAndCoWebsite/src/scripts/index.js
@@ -3,6 +3,8 @@ let Quotes = [
     '"Never finished a project since \'08"', '"Commiting tax fraud since \'98"', '"I just want to Grill"', '"I don\'t play games.... Unless I want to"', '"Comedic is comedy but I\'m bad at spelling'
 ];
 
+var visits = 0;
+
 function selectRandomQuote() {
     let ranNum = Math.floor(Math.random() * Quotes.length + 1);
 
@@ -21,5 +23,8 @@ function selectRandomQuote() {
 let randomQuote = selectRandomQuote();
 
 window.onload = function(){
+    visits++;
+
     document.getElementById("ranQuote").innerHTML = randomQuote;
+    document.getElementById("numOfVisits").innerHTML = "Number of Employees: " + visits;
 }
