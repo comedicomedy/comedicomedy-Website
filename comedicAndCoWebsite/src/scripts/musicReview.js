621d87164872bd33224c031c01fc396e7fba43b1
comedicomedy <88862122+comedicomedy@users.noreply.github.com>
2024-08-04 00:40:25 -0400

    Makes Sure Image Only Loads Once in musicReview.js

--- comedicAndCoWebsite/src/scripts/musicReview.js
+++ comedicAndCoWebsite/src/scripts/musicReview.js
@@ -1,14 +1,21 @@
 // Albums
+var isLoaded = false;
 
 window.addEventListener("DOMContentLoaded", () => {
     const albums = { tpab_klamar: document.getElementById("To Pimp A Butterfly") }
     
-    albums.tpab_klamar.addEventListener("mouseenter", () => {
-       albums.tpab_klamar.innerHTML = 
-       '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
-           
+    albums.tpab_klamar.addEventListener("mouseover", () => {
+        isLoaded = true;                
     });
+
+    if (isLoaded === true) {
+        albums.tpab_klamar.innerHTML = 
+        '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
+    }
+    
     albums.tpab_klamar.addEventListener("mouseout", () => {
+        isLoaded = false;
+
         albums.tpab_klamar.innerHTML =  
         '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">'
     });
