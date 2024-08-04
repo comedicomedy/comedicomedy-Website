f0527dca52e913c96f97bcb78cd0510c63c719d8
comedicomedy <88862122+comedicomedy@users.noreply.github.com>
2024-08-04 00:53:41 -0400

    Added More Tweaks

--- comedicAndCoWebsite/src/scripts/musicReview.js
+++ comedicAndCoWebsite/src/scripts/musicReview.js
@@ -4,22 +4,22 @@ var isLoaded = false;
 window.addEventListener("DOMContentLoaded", () => {
     const albums = { tpab_klamar: document.getElementById("To Pimp A Butterfly") }
     
-    albums.tpab_klamar.addEventListener("mouseover", () => {
+    albums.tpab_klamar.addEventListener("mouseenter", () => {
         isLoaded = true;                
-    });
+    });    
 
-    
-
-    albums.tpab_klamar.addEventListener("mouseout", () => {
-        isLoaded = false;
-
-        albums.tpab_klamar.innerHTML =  
-        '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">'
+    albums.tpab_klamar.addEventListener("mouseleave", () => {
+        isLoaded = false;        
     });
     
 })
 
-if (isLoaded == true) {
+if (isLoaded === true) {
     albums.tpab_klamar.innerHTML = 
     '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
+}
+
+if (isLoaded === false){
+    albums.tpab_klamar.innerHTML =  
+    '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">'
 }
