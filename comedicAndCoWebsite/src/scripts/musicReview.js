2670946b71c36b3890238320ed368617df781f44
comedicomedy <88862122+comedicomedy@users.noreply.github.com>
2024-08-04 01:05:42 -0400

    fixed issue with album cover not working.

--- comedicAndCoWebsite/src/scripts/musicReview.js
+++ comedicAndCoWebsite/src/scripts/musicReview.js
@@ -1,25 +1,24 @@
 // Albums
+
 var isLoaded = false;
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", () => {        
     const albums = { tpab_klamar: document.getElementById("To Pimp A Butterfly") }
-    
+
+
     albums.tpab_klamar.addEventListener("mouseenter", () => {
         isLoaded = true;                
+        console.log("true");
+        albums.tpab_klamar.innerHTML = 
+        '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
     });    
 
     albums.tpab_klamar.addEventListener("mouseleave", () => {
         isLoaded = false;        
+        albums.tpab_klamar.innerHTML =  
+        '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">'
+
     });
-    
+
+       
 })
-
-if (isLoaded === true) {
-    albums.tpab_klamar.innerHTML = 
-    '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
-}
-
-if (isLoaded === false){
-    albums.tpab_klamar.innerHTML =  
-    '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">'
-}
