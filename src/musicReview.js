419e6380d66243b8154003457a2f082e8c05d59c
comedicomedy <git@comedicomedy.mozmail.com>
2024-11-15 00:43:37 -0500

    added may it never falter album review. Changed about me on index.html. Switched twitter for blue sky on socials.html. Also added sir shouter youtube link

--- src/musicReview.js
+++ src/musicReview.js
@@ -2,23 +2,32 @@
 
 var isLoaded = false;
 
-window.addEventListener("DOMContentLoaded", () => {        
-    const albums = { tpab_klamar: document.getElementById("To Pimp A Butterfly") }
+window.addEventListener("DOMContentLoaded", () => {
+  const albums = {
+    tpab_klamar: document.getElementById("To Pimp A Butterfly"),
+    minf_glaive: document.getElementById("May it Never Falter"),
+  };
 
+  albums.tpab_klamar.addEventListener("mouseenter", () => {
+    isLoaded = true;
+    console.log("true");
+    albums.tpab_klamar.innerHTML =
+      '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
+  });
 
-    albums.tpab_klamar.addEventListener("mouseenter", () => {
-        isLoaded = true;                
-        console.log("true");
-        albums.tpab_klamar.innerHTML = 
-        '<a href="musicReviews/tpab.html" ><img src="assets/albumCovers/TPAB.png" alt="To Pimp a Butterfly" id="tpab-klamar" class="albumCoverGrid"></a>';
-    });    
+  albums.tpab_klamar.addEventListener("mouseleave", () => {
+    isLoaded = false;
+    albums.tpab_klamar.innerHTML =
+      '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">';
+  });
 
-    albums.tpab_klamar.addEventListener("mouseleave", () => {
-        isLoaded = false;        
-        albums.tpab_klamar.innerHTML =  
-        '<img src="assets/albumCovers/kLamar-TPAB.png" alt="To Pimp a Butterfly" class="albumCoverGrid" "id="tpab-klamar">'
+  albums.minf_glaive.addEventListener("mouseenter", () => {
+    albums.minf_glaive.innerHTML =
+      '<a href="musicReviews/minf.html" ><img src="assets/albumCovers/MayItNeverFalter.png" alt="May it Never Falter by Glaive Album Cover" id="minf-glaive" class="albumCoverGrid"></a>';
+  });
 
-    });
-
-       
-})
+  albums.minf_glaive.addEventListener("mouseleave", () => {
+    albums.minf_glaive.innerHTML =
+      '<a href="musicReviews/minf.html" ><img src="assets/albumCovers/glaive-MayItNeverFalter.jpg" alt="May it Never Falter by Glaive Album Cover" id="minf-glaive" class="albumCoverGrid"></a>';
+  });
+});
